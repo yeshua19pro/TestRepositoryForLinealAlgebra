2441ba78322f107c5a58331d912382e477b5050a
Miguel Angel <miguel.higuitaserrano@inemjose.edu.co>
2025-09-19 17:09:23 -0500

    Mis cambios en Main

--- Teorema34.java
+++ Teorema34.java
@@ -1,5 +1,3 @@
-
-
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -16,7 +14,12 @@
  */
 import java.util.Scanner;
 
-public class Teorema34 {   
+public class Teorema34 extends Teoremas {   
+    public Teorema34(int[][] matriz) {
+        super(matriz);
+        //TODO Auto-generated constructor stub
+    }
+
     /**
      * Metodo principal que crea una matriz cuadrada del tamaño que desea el usuario 
      * dejando la segunda columna llena de ceros para cumplir con las condiciones que 
@@ -28,72 +31,83 @@ public class Teorema34 {
     public static void main(String[] args) {
        
         //crea matriz n x n en base al input del usuario
-       
         Scanner scanner = new Scanner(System.in);
         System.out.println("Ingresa el tamaño de la matriz cuadrada");
         int n = scanner.nextInt();
-        int [][] matrix = new int[n][n];
-        
+        int [][] matriz = new int[n][n];
         
         //llena matriz
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if(j == 1) {
-                    matrix[i][j] = 0;
+                    matriz[i][j] = 0;
                 } else {
-                    matrix[i][j] = (i + 1) * (j + 2);
-}
-}
-}
-             //imprime matriz
+                    matriz[i][j] = (i + 1) * (j + 2);
+                }
+            }
+        }
+
+        // Crear objeto y aplicar el teorema
+        Teorema34 t34 = new Teorema34(matriz);
+        t34.aplicar();
+    }
+
+    @Override
+    public void aplicar() {
+        int n = matriz.length;
+
+        //imprime matriz
         System.out.println("Matriz:");
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                System.out.print(matrix[i][j] + " ");
+                System.out.print(matriz[i][j] + " ");
             }
             System.out.println(" ");
-}
-        //Checar si la matriz tiene columna o fila de ceros
-            boolean tieneColumnaCeros = false;
-            for (int j = 0; j < n; j++) {
-                boolean columnaCeros = true;
-                for (int i = 0; i < n; i++) {
-                    if(matrix[i][j]!= 0) {
-                        columnaCeros = false;
-                        break;
-                    }
-                }
-                    if(columnaCeros) {
-                        tieneColumnaCeros = true;
-                        break;
-                    }
-                }
-            boolean tieneFilaCeros = false;
+        }
+
+        //Checar si la matriz tiene columna de ceros
+        boolean tieneColumnaCeros = false;
+        for (int j = 0; j < n; j++) {
+            boolean columnaCeros = true;
             for (int i = 0; i < n; i++) {
-                boolean filaCeros = true;
-                for (int j = 0; j < n; j++) {
-                       if (matrix[i][j] != 0) {
-                            filaCeros = false;
-                            break;
-                        }
-                    }
-                    if (filaCeros) {
-                        tieneFilaCeros = true;
-                        break;
-                        }
-                    }
-                    //calcula el determinante usando clase externa llamada Determinante
-            int det = Determinante.determinante(matrix);
-            System.out.println("Determinante de la matriz: " + det);
-            
-            //Evalua el Teorema 3.4
-            if ((tieneFilaCeros || tieneColumnaCeros) && det == 0) {
-                System.out.println("Se cumple el Teorema 3.4: Determinante = 0 al presentarse una fila o columna de ceros");
-            } else if ((tieneFilaCeros || tieneColumnaCeros) && det != 0) {
-                System.out.println("El Teorema 3.4 no se cumple");
-            } else {
-                System.out.println("El determinante es distante de cero y no hay fila o columna de ceros");
-                
+                if(matriz[i][j]!= 0) {
+                    columnaCeros = false;
+                    break;
+                }
+            }
+            if(columnaCeros) {
+                tieneColumnaCeros = true;
+                break;
             }
         }
+
+        //Checar si la matriz tiene fila de ceros
+        boolean tieneFilaCeros = false;
+        for (int i = 0; i < n; i++) {
+            boolean filaCeros = true;
+            for (int j = 0; j < n; j++) {
+                if (matriz[i][j] != 0) {
+                    filaCeros = false;
+                    break;
+                }
+            }
+            if (filaCeros) {
+                tieneFilaCeros = true;
+                break;
+            }
+        }
+
+        //calcula el determinante usando clase externa llamada Determinante
+        int det = Tools.determinante(matriz);
+        System.out.println("Determinante de la matriz: " + det);
+            
+        //Evalua el Teorema 3.4
+        if ((tieneFilaCeros || tieneColumnaCeros) && det == 0) {
+            System.out.println("Se cumple el Teorema 3.4: Determinante = 0 al presentarse una fila o columna de ceros");
+        } else if ((tieneFilaCeros || tieneColumnaCeros) && det != 0) {
+            System.out.println("El Teorema 3.4 no se cumple");
+        } else {
+            System.out.println("El determinante es distante de cero y no hay fila o columna de ceros");
+        }
     }
+}
